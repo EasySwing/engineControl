4ed2031bed78ea9578b40a67e296c50c9e4074f6
jonakemon <jonathan@seib.nl>
2021-01-26 14:30:21 +0100

    Homeing per motor

--- homeing.py
+++ homeing.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-from config import setup_pins, ROTATION_PULSES
+from config import setup_pins, MOTORS
 
 setup_pins()
 
@@ -9,15 +9,16 @@ def home_yet(PIN):
     return GPIO.input(PIN)
 
 try:
-    while True:
-        GPIO.output(ROTATION_PIN, 1)
-        sleep(0.001)
-        GPIO.output(ROTATION_PIN, 0)
-        sleep(0.001)
+    for motor in MOTORS:
+        while True:
+            GPIO.output(motor.get('ROTATION_PIN'), 1)
+            sleep(0.001)
+            GPIO.output(motor.get('ROTATION_PIN'), 0)
+            sleep(0.001)
 
-        if home_yet(HOME):
-            print('home! :)')
-            break
+            if home_yet(motor.get('HOME_SWITCH')):
+                print('home! :)')
+                break
 
 
 except KeyboardInterrupt:
